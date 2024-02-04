030174140ecddab04606e3f976a474b6b94ad46a
JoseABermudez57 <jose.apep570@gmail.com>
2024-02-04 11:27:09 -0600

    [UPDATE] Main view

--- views/gui.py
+++ views/gui.py
@@ -1,9 +1,15 @@
 from pathlib import Path
-from tkinter import Tk, Canvas, Entry, Button, PhotoImage
-
+from tkinter import Tk, Canvas, Entry, Label, Button, PhotoImage, filedialog
+from PIL import Image, ImageTk
+from operations.neuron import open_csv as start
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\josea\OneDrive\Documentos\Pruebas VSC\Tkinter-Designer-master\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"..\views\assets\frame0")
+csv_route = ""
+csv_opened = False
+eta = ''
+learning_rate = ''
+iterations = ''
 
 
 def relative_to_assets(path: str) -> Path:
@@ -11,7 +17,7 @@ def relative_to_assets(path: str) -> Path:
 
 
 window = Tk()
-
+window.title("Neuron Simulation")
 window.geometry("500x500")
 window.configure(bg = "#E5E5E5")
 
@@ -38,150 +44,94 @@ image_1 = canvas.create_image(
 image_image_2 = PhotoImage(
     file=relative_to_assets("image_2.png"))
 image_2 = canvas.create_image(
-    211.0,
-    262.0,
+    208.0,
+    202.0,
     image=image_image_2
 )
 
 image_image_3 = PhotoImage(
     file=relative_to_assets("image_3.png"))
 image_3 = canvas.create_image(
-    194.0,
-    206.0,
+    449.0,
+    204.0,
     image=image_image_3
 )
 
 image_image_4 = PhotoImage(
     file=relative_to_assets("image_4.png"))
 image_4 = canvas.create_image(
-    128.0,
-    147.0,
+    447.0,
+    202.0,
     image=image_image_4
-)
-
-image_image_5 = PhotoImage(
-    file=relative_to_assets("image_5.png"))
-image_5 = canvas.create_image(
-    357.0,
-    147.0,
-    image=image_image_5
-)
-
-image_image_6 = PhotoImage(
-    file=relative_to_assets("image_6.png"))
-image_6 = canvas.create_image(
-    355.0,
-    144.0,
-    image=image_image_6
-)
-
-image_image_7 = PhotoImage(
-    file=relative_to_assets("image_7.png"))
-image_7 = canvas.create_image(
-    439.0,
-    208.0,
-    image=image_image_7
-)
-
-image_image_8 = PhotoImage(
-    file=relative_to_assets("image_8.png"))
-image_8 = canvas.create_image(
-    438.0,
-    205.0,
-    image=image_image_8
-)
-
-image_image_9 = PhotoImage(
-    file=relative_to_assets("image_9.png"))
-image_9 = canvas.create_image(
-    451.0,
-    264.0,
-    image=image_image_9
-)
-
-image_image_10 = PhotoImage(
-    file=relative_to_assets("image_10.png"))
-image_10 = canvas.create_image(
-    449.0,
-    262.0,
-    image=image_image_10
 )
 
 entry_image_1 = PhotoImage(
     file=relative_to_assets("entry_1.png"))
 entry_bg_1 = canvas.create_image(
-    450.5,
-    262.5,
+    448.5,
+    202.5,
     image=entry_image_1
 )
 entry_1 = Entry(
     bd=0,
     bg="#EEEEEE",
     fg="#000716",
-    highlightthickness=0
+    highlightthickness=0,
+    font=('Comic Sans MS', 12, 'normal')
 )
 entry_1.place(
-    x=423.0,
-    y=252.0,
+    x=421.0,
+    y=192.0,
     width=55.0,
     height=19.0
 )
 
-entry_image_2 = PhotoImage(
-    file=relative_to_assets("entry_2.png"))
-entry_bg_2 = canvas.create_image(
-    441.5,
-    205.5,
-    image=entry_image_2
-)
-entry_2 = Entry(
-    bd=0,
-    bg="#EEEEEE",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_2.place(
-    x=401.0,
-    y=194.0,
-    width=81.0,
-    height=21.0
-)
-
-entry_image_3 = PhotoImage(
-    file=relative_to_assets("entry_3.png"))
-entry_bg_3 = canvas.create_image(
-    354.0,
-    145.0,
-    image=entry_image_3
-)
-entry_3 = Entry(
-    bd=0,
-    bg="#EEEEEE",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_3.place(
-    x=282.0,
-    y=133.0,
-    width=144.0,
-    height=22.0
-)
-
-image_image_11 = PhotoImage(
-    file=relative_to_assets("image_11.png"))
-image_11 = canvas.create_image(
+image_image_5 = PhotoImage(
+    file=relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(
     255.0,
-    423.0,
-    image=image_image_11
+    392.0,
+    image=image_image_5
 )
 
-image_image_12 = PhotoImage(
-    file=relative_to_assets("image_12.png"))
-image_12 = canvas.create_image(
+image_image_6 = PhotoImage(
+    file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(
     250.0,
-    414.0,
-    image=image_image_12
+    387.0,
+    image=image_image_6
 )
+
+image_correct = Image.open(Path(r"..\views\assets\frame0\correct.png")).resize((27, 27))
+image_tk_correct = ImageTk.PhotoImage(image_correct)
+
+
+image_incorrect = Image.open(Path(r"..\views\assets\frame0\error.png")).resize((27, 27))
+image_tk_incorrect = ImageTk.PhotoImage(image_incorrect)
+
+
+def search_csv():
+    global csv_route, csv_opened
+    file = filedialog.askopenfilename(filetypes=[("Archivo CSV", "*.csv")])
+    if file:
+        try:
+            csv_route = file
+            csv_opened = True
+            Label(window, image=image_tk_correct).place(x=445, y=75)
+        except Exception as e:
+            print(f"Error al cargar: \n{e}")
+    else:
+        csv_opened = False
+        Label(window, image=image_tk_incorrect).place(x=445, y=75)
+
+
+def do_operations():
+    if csv_opened:
+        Label(window, image=image_tk_correct).place(x=330, y=305)
+        start(csv_route, entry_2.get(), entry_1.get())
+    else:
+        Label(window, image=image_tk_incorrect).place(x=330, y=258)
+
 
 button_image_1 = PhotoImage(
     file=relative_to_assets("button_1.png"))
@@ -189,12 +139,12 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_1 clicked"),
+    command=do_operations,
     relief="flat"
 )
 button_1.place(
     x=175.0,
-    y=299.0,
+    y=250.0,
     width=151.0,
     height=47.0
 )
@@ -205,7 +155,7 @@ button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
+    command=search_csv,
     relief="flat"
 )
 button_2.place(
@@ -215,28 +165,60 @@ button_2.place(
     height=40.0
 )
 
-canvas.create_rectangle(
-    0.0,
-    0.0,
-    500.0,
-    55.0,
-    fill="#646464",
-    outline="")
-
-image_image_13 = PhotoImage(
-    file=relative_to_assets("image_13.png"))
-image_13 = canvas.create_image(
-    205.0,
-    29.0,
-    image=image_image_13
+image_image_7 = PhotoImage(
+    file=relative_to_assets("image_7.png"))
+image_7 = canvas.create_image(
+    250.0,
+    28.0,
+    image=image_image_7
 )
 
-image_image_14 = PhotoImage(
-    file=relative_to_assets("image_14.png"))
-image_14 = canvas.create_image(
-    423.0,
-    29.0,
-    image=image_image_14
+image_image_8 = PhotoImage(
+    file=relative_to_assets("image_8.png"))
+image_8 = canvas.create_image(
+    161.0,
+    144.0,
+    image=image_image_8
 )
-window.resizable(False, False)
-window.mainloop()
+
+image_image_9 = PhotoImage(
+    file=relative_to_assets("image_9.png"))
+image_9 = canvas.create_image(
+    378.0,
+    146.0,
+    image=image_image_9
+)
+
+image_image_10 = PhotoImage(
+    file=relative_to_assets("image_10.png"))
+image_10 = canvas.create_image(
+    377.0,
+    143.0,
+    image=image_image_10
+)
+
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
+    380.5,
+    143.5,
+    image=entry_image_2
+)
+entry_2 = Entry(
+    bd=0,
+    bg="#EEEEEE",
+    fg="#000716",
+    highlightthickness=0,
+    font=('Comic Sans MS', 12, 'normal')
+)
+entry_2.place(
+    x=340.0,
+    y=132.0,
+    width=81.0,
+    height=21.0
+)
+
+if __name__ == '__main__':
+    window.resizable(False, False)
+    window.mainloop()
+
